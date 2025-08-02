2ee51979235a674d63a3d8c9381f37ccc84ecfd7
Ismail Ceylan <ismaillceylan@gmail.com>
2025-08-02 14:30:04 +0300

    feat(helpers): add new types/is-string helper

---
+++ helpers/types/is-string/README.md
@@ -0,0 +1,12 @@
+# isString
+Checks if a value is a string.
+
+---
+
+## Usage
+```js
+import { isString } from "mark-3/helpers/types";
+
+isString( "foo" ); // true
+isString(1); // false
+```
---
+++ helpers/types/is-string/index.js
@@ -0,0 +1,10 @@
+/**
+ * Checks if a value is a string.
+ * 
+ * @param {*} value
+ * @return {boolean}
+ */
+export default function isString( value )
+{
+	return typeof value === "string";
+}
