087af8061b5aeb284c56b95212cc39f1166f5ff1
Ismail Ceylan <ismaillceylan@gmail.com>
2025-08-02 14:30:34 +0300

    chores: update index files for new helpers

---
+++ helpers/string/index.js
@@ -0,0 +1,3 @@
+export { default as trim } from "./trim";
+export { default as escapeRegex } from "./escape-regex";
+export { default as camelToDash } from "./camel-to-dash";
---
+++ helpers/types/index.js
@@ -0,0 +1,4 @@
+export { default as getTypeName } from "./get-type-name";
+export { default as isString } from "./is-string";
+export { default as isEmpty } from "./is-empty";
+export { default as isArray } from "./is-array";
