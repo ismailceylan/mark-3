fce811a0ef9135673230445df726e40e279b0661
Ismail Ceylan <ismaillceylan@gmail.com>
2025-08-28 19:35:48 +0300

    refactor(helpers): ensure that the throttle helper handles trailing and leading conditions

--- helpers/time/throttle.js
+++ helpers/time/throttle.js
@@ -11,20 +11,53 @@
  * 
  * @param {function} func The function to throttle.
  * @param {number} delay The delay in milliseconds.
+ * @param {object} options - The options for throttling.
+ * @param {boolean} [options.leading=true] - Whether to invoke the function on the leading edge of the delay.
+ * @param {boolean} [options.trailing=true] - Whether to invoke the function on the trailing edge of the delay.
  * @returns {function} A throttled version of the input function.
  */
-export default function throttle( func, delay )
+export default function throttle( func, delay, { leading = true, trailing = true } = {})
 {
 	let lastCall = 0;
+	let timeoutId = null;
+	let lastArgs;
+	let lastThis;
 
 	return function( ...args )
 	{
 		const now = Date.now();
+		
+		lastArgs = args;
+		lastThis = this;
 
-		if( now - lastCall >= delay )
+		const invoke = () =>
+		{
+			lastCall = Date.now();
+			func.apply( lastThis, lastArgs );
+			timeoutId = null;
+		}
+
+		if( lastCall === 0 && ! leading )
 		{
 			lastCall = now;
-			func.apply( this, args );
+		}
+
+		const remaining = delay - ( now - lastCall );
+
+		if( remaining <= 0 )
+		{
+			if( timeoutId )
+			{
+				clearTimeout( timeoutId );
+				timeoutId = null;
+			}
+
+			invoke();
+		}
+		else if( trailing && ! timeoutId )
+		{
+			// Trailing için timeout ayarla
+			timeoutId = setTimeout( invoke, remaining );
 		}
 	}
 }
