15273cb7ade8be17a5dda74d8402dbd1f1371b5c
Ismail Ceylan <ismaillceylan@gmail.com>
2025-09-11 13:04:55 +0300

    fix(helpers): correct from-timestamp filename without affecting exports

--- helpers/date/index.js
+++ helpers/date/index.js
@@ -1,4 +1,4 @@
 export { default as format } from "./format";
 export { default as isOnSameDay } from "./is-on-same-day";
-export { default as fromTimestamp } from "./format-timestamp";
+export { default as fromTimestamp } from "./from-timestamp";
 export { default as isOnSameYear } from "./is-on-same-year";
