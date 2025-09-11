f177f9185982c1c4c1211ee12a0fb7b11144a721
Ismail Ceylan <ismaillceylan@gmail.com>
2025-09-11 12:19:39 +0300

    refactor(composables): move change listener into options area and typify

--- composables/use-media-query.ts
+++ composables/use-media-query.ts
@@ -5,19 +5,16 @@ import { useEventListener } from ".";
  * Creates a reactive reference that tracks whether the given media query matches the current viewport.
  *
  * @param query - A valid media query string.
- * @param watcher - A function that will be called whenever the match status changes.
- * The function will receive the current match status as a boolean argument.
- * @param options.persistent - Whether to persist the match status across component mounts and unmounts.
  * @returns A reactive reference to the match status.
  * @example
- * const isMobileRef = useMediaQuery( "(max-width: 767px)", matches =>
- *    console.log( matches )
- * );
+ * const isMobileRef = useMediaQuery( "(max-width: 767px)",
+ * {
+ *     onChange: matches => console.log( matches )
+ * });
  */
 export default function useMediaQuery(
 	query: string,
-	watcher?: ( matches: boolean) => void,
-	{ persistent = false } = {}
+	{ persistent = false, onChange }: UseMediaQueryOptions = {}
 ): Ref<boolean>
 {
 	const match = matchMedia( query );
@@ -33,8 +30,22 @@ export default function useMediaQuery(
 
 	function triggerWatcher( _?: boolean )
 	{
-		watcher && watcher( matches.value );
+		onChange && onChange( matches.value );
 	}
 
 	return matches;
+}
+
+type ChangeListener = ( matches: boolean ) => void;
+
+interface UseMediaQueryOptions
+{
+	/** Whether to persist the match status across component mounts and unmounts */
+	persistent?: boolean;
+	/**
+	 * A function that will be called whenever the match status
+	 * changes. The function will receive the current match status
+	 * as a boolean argument.
+	 */
+	onChange?: ChangeListener;
 }
