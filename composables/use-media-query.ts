990889152e36ea87b38c87066a04366b98b7146a
Ismail Ceylan <ismaillceylan@gmail.com>
2025-09-11 11:51:16 +0300

    refactor(composables): convert use-media-query to typescript

--- composables/use-media-query.ts
+++ composables/use-media-query.ts
@@ -1,31 +1,33 @@
-import { ref } from "vue";
+import { type Ref, ref } from "vue";
 import { useEventListener } from ".";
 
 /**
  * Creates a reactive reference that tracks whether the given media query matches the current viewport.
  *
- * @typedef {import('vue').Ref} Ref
- * @param {string} query - A valid media query string.
- * @param {function(boolean)} [watcher] - A function that will be called whenever the match status changes.
+ * @param query - A valid media query string.
+ * @param watcher - A function that will be called whenever the match status changes.
  * The function will receive the current match status as a boolean argument.
- * @returns {Ref<boolean>} A reactive reference to the match status.
+ * @returns A reactive reference to the match status.
  * @example
  * const isMobileRef = useMediaQuery( "(max-width: 767px)", matches =>
  *    console.log( matches )
  * );
  */
-export default function useMediaQuery( query, watcher )
+export default function useMediaQuery(
+	query: string,
+	watcher?: ( matches: boolean) => void
+): Ref<boolean>
 {
 	const match = matchMedia( query );
 	const matches = ref( match.matches );
 
 	triggerWatcher();
 
-	useEventListener( match, "change", e =>
+	useEventListener( match, "change", ( e: MediaQueryListEvent ) =>
 		triggerWatcher( matches.value = e.matches )
 	);
 
-	function triggerWatcher()
+	function triggerWatcher( _?: boolean )
 	{
 		watcher && watcher( matches.value );
 	}
