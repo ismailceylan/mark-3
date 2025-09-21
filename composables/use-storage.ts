c15be926caf96007493893df955306a18eab2aff
Ismail Ceylan <ismaillceylan@gmail.com>
2025-09-21 23:57:24 +0300

    refactor(composables): rebuild use-storage composable

--- composables/use-storage.ts
+++ composables/use-storage.ts
@@ -1,17 +1,18 @@
 /**
- * A hook for interacting with the storage.
- * 
+ * A hook for interacting with the storage with a lock mechanism.
+ *
  * @param key - The key to be used for the storage item.
  * @param storage - The storage object to be used. Defaults to localStorage.
- * @returns An object with methods for setting, getting, and removing the storage
- * item associated with the given key.
  */
-export default function useStorage<T>( key: string, storage: Storage = localStorage )
+export default function useStorage<T>(
+	key: string,
+	storage: Storage = localStorage
+)
 {
 	/**
-	 * Set the value of the storage item associated with the given key.
-	 * 
-	 * @param value - The value to be stored.
+	 * Sets the value of the storage item with the given key.
+	 *
+	 * @param value - The value to be set.
 	 */
 	function set( value: T )
 	{
@@ -19,28 +20,54 @@ export default function useStorage<T>( key: string, storage: Storage = localStor
 	}
 
 	/**
-	 * Retrieve the value associated with the given key from the storage.
-	 * If there is no such key, return the given default value.
-	 * 
-	 * @param defaultValue - The value to be returned if there is no such key.
-	 * @returns The value associated with the given key if it exists, otherwise the given default value.
+	 * Gets the value of the storage item with the given key.
+	 *
+	 * @param defaultValue - The default value to be returned if the storage item does not exist.
+	 * @returns The value of the storage item if it exists, otherwise the default value.
 	 */
-	function get( defaultValue?: T ): T
+	function get( defaultValue?: T ): T | undefined
 	{
 		const item = storage.getItem( key );
-		
+
 		return item
 			? JSON.parse( item ) as T
 			: defaultValue;
 	}
 
 	/**
-	 * Remove the item associated with the given key from the storage.
+	 * Removes the storage item.
 	 */
 	function remove()
 	{
 		storage.removeItem( key );
 	}
 
-	return { set, get, remove }
+	/**
+	 * Watches for changes to the storage item with the given key.
+	 *
+	 * @param callback - The callback function to be called when the storage item changes.
+	 * @param options - An object containing the options for the watcher.
+	 * @param options.immediate - If true, the callback will be called immediately with the current value of the storage item.
+	 */
+	function watch( callback: ( value: T | undefined ) => void, { immediate = false } = {})
+	{
+		window.addEventListener( "storage", e =>
+		{
+			if( e.key === key && e.newValue !== e.oldValue )
+			{
+				callback(
+					e.newValue
+						? JSON.parse( e.newValue ) as T
+						: undefined
+				);
+			}
+		});
+
+		if( immediate )
+		{
+			callback( get());
+		}
+	}
+
+	return { set, get, remove, watch };
 }
