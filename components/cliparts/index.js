b1535a1033ad80321cccd962bc3ee6b2a0af2ac8
Ismail Ceylan <ismaillceylan@gmail.com>
2025-12-29 19:38:19 +0300

    feat(cliparts): add exports for various clipart components

---
+++ components/cliparts/index.js
@@ -0,0 +1,6 @@
+export { default as UnderConstruction } from "./under-construction.vue";
+export { default as OfflineShore } from "./offline-shore.vue";
+export { default as PublishPost } from "./publish-post.vue";
+export { default as Error404 } from "./error-404.vue";
+export { default as MyAnswer } from "./my-answer.vue";
+export { default as Server } from "./server.vue";
