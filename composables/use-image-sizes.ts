17965d320fe1bbc8c4cdddbbd3bdf40ca4d5301b
Ismail Ceylan <ismaillceylan@gmail.com>
2026-01-15 14:00:18 +0300

    feat(use-image-sizes): include isLoaded state in return values

--- composables/use-image-sizes.ts
+++ composables/use-image-sizes.ts
@@ -4,20 +4,22 @@ import { ref } from "vue";
  * Returns the natural width and height of an image, along with a
  * onLoad function that can be used to load the image.
  * 
- * @returns width, height and onLoad function
+ * @returns width, height, isLoaded and onLoad function
  */
 export default function useImageSizes()
 {
 	const width = ref( 16 );
 	const height = ref( 9 );
+	const isLoaded = ref( false );
 
 	function onLoad( event: Event )
 	{
 		const img = event.target as HTMLImageElement;
 
+		isLoaded.value = true;
 		width.value = img.naturalWidth;
 		height.value = img.naturalHeight;
 	}
 
-	return { width, height, onLoad }
+	return { width, height, isLoaded, onLoad }
 }
